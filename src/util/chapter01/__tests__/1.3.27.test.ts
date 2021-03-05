b70d87fbb0d19a50d8252ba4d50359ea5e448e39
rxliuli <rxliuli@gmail.com>
2021-03-05 11:47:44 +0800

    实现 1.3.28

--- src/util/chapter01/__tests__/1.3.27.test.ts
+++ src/util/chapter01/__tests__/1.3.27.test.ts
@@ -6,13 +6,59 @@ import { LinkedNodeUtil } from '../LinkedNodeUtil'
 import { LinkedNode } from '../LinkedNode'
 
 it('1.3.27', () => {
-  const f = (a: LinkedNode<number>, b: LinkedNode<number>) => a.value - b.value
-  expect(LinkedNodeUtil.max(null, f)).toBeNull()
-  const node: LinkedNode<number> = Array(5)
-    .fill(0)
-    .reduce(
-      (res, _, i) => ({ value: 5 - i, next: res } as LinkedNode<number>),
-      null,
-    )
-  expect(LinkedNodeUtil.max(node, f)).toBe(5)
+  function testMax(
+    max: <T, N extends LinkedNode<T> | null>(
+      node: N,
+      predicate: (a: LinkedNode<T>, b: LinkedNode<T>) => number,
+    ) => N,
+  ) {
+    const f = (a: LinkedNode<number>, b: LinkedNode<number>) =>
+      a.value - b.value
+    expect(max(null, f)).toBeNull()
+    const node: LinkedNode<number> = Array(5)
+      .fill(0)
+      .reduce(
+        (res, _, i) => ({ value: 5 - i, next: res } as LinkedNode<number>),
+        null,
+      )
+    expect(max(node, f)).toBe(5)
+  }
+
+  /**
+   * 使用递归调用
+   * @param node
+   * @param predicate
+   */
+  function max<T>(
+    node: LinkedNode<T>,
+    predicate: (a: LinkedNode<T>, b: LinkedNode<T>) => number,
+  ): T
+  function max<T>(
+    node: null,
+    predicate: (a: LinkedNode<T>, b: LinkedNode<T>) => number,
+  ): null
+  function max<T>(
+    node: LinkedNode<T> | null,
+    predicate: (a: LinkedNode<T>, b: LinkedNode<T>) => number,
+  ): T | null {
+    if (node === null) {
+      return null
+    }
+    let max = node
+
+    function f(item: LinkedNode<T> | null): LinkedNode<T> {
+      if (item === null) {
+        return max
+      }
+      if (predicate(max, item) < 0) {
+        max = item
+      }
+      return f(item.next)
+    }
+
+    return f(node.next).value
+  }
+
+  testMax(LinkedNodeUtil.max)
+  testMax(max)
 })
