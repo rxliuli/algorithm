c4e9a2638c851a658d47765f10140d994d499151
rxliuli <rxliuli@gmail.com>
2021-03-05 14:25:10 +0800

    实现 1.3.29

---
+++ src/util/chapter01/__tests__/1.3.29.test.ts
@@ -0,0 +1,61 @@
+/*
+1.3.29　用环形链表实现Queue。环形链表也是一条链表，只是没有任何结点的链接为空，且只要链表非空则 last.next 的值为 first。只能使用一个 Node 类型的实例变量（last）。
+ */
+
+import { IQueue } from '../Queue'
+import { LinkedNode } from '../LinkedNode'
+import { testQueue } from './Queue.test'
+
+class LoopLinkedQueue<T> implements IQueue<T> {
+  private last: LinkedNode<T> | null = null
+  private _size = 0
+
+  get isEmpty(): boolean {
+    return this.size === 0
+  }
+
+  get size(): number {
+    return this._size
+  }
+
+  *[Symbol.iterator](): Iterator<T> {
+    for (let item = this.last!.next; item; item = item.next) {
+      yield item.value
+      if (item === this.last) {
+        break
+      }
+    }
+  }
+
+  dequeue(): T | null {
+    if (this._size === 0) {
+      return null
+    }
+    const res = this.last!.next!
+    if (this._size === 1) {
+      this.last = null
+    } else {
+      this.last!.next = res.next
+    }
+    this._size--
+    return res.value
+  }
+
+  enqueue(item: T): void {
+    if (this._size === 0) {
+      this.last = { value: item, next: null }
+      this.last.next = this.last
+    } else {
+      this.last!.next = {
+        value: item,
+        next: this.last!.next,
+      }
+      this.last = this.last!.next
+    }
+    this._size++
+  }
+}
+
+it('1.3.29', () => {
+  testQueue(LoopLinkedQueue)
+})
--- src/util/chapter01/__tests__/Queue.test.ts
+++ src/util/chapter01/__tests__/Queue.test.ts
@@ -1,19 +1,19 @@
 import { IQueue, LinkedQueue, Queue } from '../Queue'
 import { Class } from 'type-fest'
 
-describe('测试 Queue', () => {
-  function testQueue(Queue: Class<IQueue<number>>) {
-    const queue = new Queue()
-    queue.enqueue(1)
-    queue.enqueue(2)
-    queue.enqueue(3)
-    expect([...queue]).toEqual([1, 2, 3])
-    expect(queue.dequeue()).toBe(1)
-    expect(queue.dequeue()).toBe(2)
-    expect(queue.dequeue()).toBe(3)
-    expect(queue.isEmpty).toBeTruthy()
-  }
+export function testQueue(Queue: Class<IQueue<number>>) {
+  const queue = new Queue()
+  queue.enqueue(1)
+  queue.enqueue(2)
+  queue.enqueue(3)
+  expect([...queue]).toEqual([1, 2, 3])
+  expect(queue.dequeue()).toBe(1)
+  expect(queue.dequeue()).toBe(2)
+  expect(queue.dequeue()).toBe(3)
+  expect(queue.isEmpty).toBeTruthy()
+}
 
+describe('测试 Queue', () => {
   it('测试 Queue', () => {
     testQueue(Queue)
   })
