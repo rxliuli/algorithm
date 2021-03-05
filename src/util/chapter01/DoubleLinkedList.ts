7221163ded3a3f062ef7558bb5b42fba2f028d0b
rxliuli <rxliuli@gmail.com>
2021-03-05 16:51:03 +0800

    实现 1.3.31

---
+++ src/util/chapter01/DoubleLinkedList.ts
@@ -0,0 +1,209 @@
+import { LinkedNodeUtil } from './LinkedNodeUtil'
+
+interface DoubleLinkedNode<T> {
+  value: T
+  next: DoubleLinkedNode<T> | null
+  prev: DoubleLinkedNode<T> | null
+}
+
+interface IDoubleLinkedList<T> extends Iterable<T> {
+  size: number
+  isEmpty: boolean
+
+  /**
+   * 在链表尾部添加
+   * @param item
+   */
+  push(item: T): void
+
+  /**
+   * 删除并返回链表尾部元素
+   */
+  pop(): T | null
+
+  /**
+   * 删除并返回链表头部元素
+   */
+  shift(): T | null
+
+  /**
+   * 在链表首位添加
+   * @param item
+   */
+  unshift(item: T): void
+}
+
+export class DoubleLinkedList<T> implements IDoubleLinkedList<T> {
+  private first: DoubleLinkedNode<T> | null = null
+  private last: DoubleLinkedNode<T> | null = null
+  private _size = 0
+
+  get size() {
+    return this._size
+  }
+
+  get isEmpty() {
+    return this.size === 0
+  }
+
+  *[Symbol.iterator](): Iterator<T> {
+    const iter = LinkedNodeUtil.iterator(this.first)
+    for (let item of iter) {
+      yield item.value
+    }
+  }
+
+  pop(): T | null {
+    if (this.size === 0) {
+      return null
+    }
+    const old = this.last!
+    this.last = old.prev
+    if (this.last === null) {
+      this.first = null
+    } else {
+      this.last.next = null
+    }
+    this._size--
+    return old.value
+  }
+
+  push(item: T): void {
+    if (this.size === 0) {
+      this.first = this.last = { value: item, prev: null, next: null }
+    } else {
+      const old = this.last
+      this.last = { value: item, prev: old, next: null }
+      old!.next = this.last
+    }
+    this._size++
+  }
+
+  shift(): T | null {
+    if (this.size === 0) {
+      return null
+    }
+    const old = this.first!
+    this.first = old.next
+    if (this.first === null) {
+      this.last = null
+    } else {
+      this.first.prev = null
+    }
+    this._size--
+    return old.value
+  }
+
+  unshift(item: T): void {
+    if (this.size === 0) {
+      this.first = this.last = { value: item, prev: null, next: null }
+    } else {
+      const old = this.first
+      this.first = { value: item, prev: null, next: old }
+      old!.prev = this.first
+    }
+    this._size++
+  }
+
+  get(index: number): T | null {
+    let i = 0
+    for (const item of this) {
+      if (i === index) {
+        return item
+      }
+      i++
+    }
+    return null
+  }
+
+  /**
+   * 在指定节点前添加新的节点
+   * @param index
+   * @param value
+   */
+  insertBefore(index: number, value: T): void {
+    const iter = LinkedNodeUtil.iterator(this.first) as Generator<
+      DoubleLinkedNode<T>
+    >
+    let i = 0
+    for (const item of iter) {
+      if (i === index) {
+        const prev = item.prev
+        const curr: DoubleLinkedNode<T> = {
+          value: value,
+          next: item,
+          prev,
+        }
+        if (prev === null) {
+          this.first = curr
+        } else {
+          prev.next = curr
+        }
+        item.prev = curr
+        this._size++
+        break
+      }
+      i++
+    }
+  }
+
+  /**
+   * 在指定节点后添加新的节点
+   * @param index
+   * @param value
+   */
+  insertAfter(index: number, value: T): void {
+    const iter = LinkedNodeUtil.iterator(this.first) as Generator<
+      DoubleLinkedNode<T>
+    >
+    let i = 0
+    for (const item of iter) {
+      if (i === index) {
+        const next = item.next
+        const curr: DoubleLinkedNode<T> = {
+          value: value,
+          next,
+          prev: item,
+        }
+        if (next === null) {
+          this.last = curr
+        } else {
+          next.prev = curr
+        }
+        item.next = curr
+        this._size++
+        break
+      }
+      i++
+    }
+  }
+
+  /**
+   * 删除指定节点
+   * @param index
+   */
+  remove(index: number): T | null {
+    const iter = LinkedNodeUtil.iterator(this.first) as Generator<
+      DoubleLinkedNode<T>
+    >
+    let i = 0
+    for (const item of iter) {
+      if (i === index) {
+        if (item.prev === null) {
+          this.first = null
+        } else {
+          item.prev.next = item.next
+        }
+        if (item.next === null) {
+          this.last = null
+        } else {
+          item.next.prev = item.prev
+        }
+        this._size--
+        return item.value
+      }
+      i++
+    }
+    return null
+  }
+}
--- src/util/chapter01/__tests__/Stack.test.ts
+++ src/util/chapter01/__tests__/Stack.test.ts
@@ -1,20 +1,21 @@
 import { IStack, LinkedStack, Stack } from '../Stack'
 import { Class } from 'type-fest'
 
-describe('测试 Stack', () => {
-  function testStack(Stack: Class<IStack<number>>) {
-    const stack = new Stack()
-    stack.push(1)
-    stack.push(2)
-    stack.push(3)
-    expect([...stack]).toEqual([3, 2, 1])
-    expect(stack.size).toBe(3)
-    expect(stack.pop()).toBe(3)
-    expect(stack.pop()).toBe(2)
-    expect(stack.pop()).toBe(1)
-    expect(stack.isEmpty).toBeTruthy()
-  }
+export function testStack(Stack: Class<IStack<number>>) {
+  const stack = new Stack()
+  stack.push(1)
+  stack.push(2)
+  stack.push(3)
+  expect([...stack]).toEqual([3, 2, 1])
+  expect(stack.size).toBe(3)
+  expect(stack.pop()).toBe(3)
+  expect(stack.pop()).toBe(2)
+  expect(stack.pop()).toBe(1)
+  expect(stack.pop()).toBeNull()
+  expect(stack.isEmpty).toBeTruthy()
+}
 
+describe('测试 Stack', () => {
   it('测试迭代器', () => {
     testStack(Stack)
   })
