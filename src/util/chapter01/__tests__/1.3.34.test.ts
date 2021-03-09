1cc457ca5dbb6cae82bf6c8e94fb22fc73ce0ac0
rxliuli <rxliuli@gmail.com>
2021-03-09 10:57:03 +0800

    跳过 1.3.34-1.3.36 的随机遍历的问题

---
+++ src/util/chapter01/__tests__/1.3.34.test.ts
@@ -0,0 +1,50 @@
+/*
+1.3.34　随机背包。随机背包能够存储一组元素并支持表 1.3.10 中的 API：
+
+　　　表 1.3.10　泛型随机背包的 API
+
+public class RandomBag<Item> implements Iterable<Item>
+             RandomBag()	创建一个空随机背包
+   boolean   isEmpty()	背包是否为空
+       int   size()	背包中的元素数量
+      void   add(Item item)	添加一个元素
+　　　编写一个RandomBag 类来实现这份API。请注意，除了形容词随机之外，这份API 和 Bag 的API 是相同的，这意味着迭代应该随机访问背包中的所有元素（对于每次迭代，所有的 N! 种排列出现的可能性均相同）。提示：用数组保存所有元素并在迭代器的构造函数中随机打乱它们的顺序。
+
+TODO 应该有某种更好的方式
+ */
+
+import { IBag } from '../Bag'
+import { sortBy } from '@liuli-util/array'
+
+class RandomBag<T> implements IBag<T> {
+  private arr: T[] = []
+  private _size = 0
+
+  get isEmpty(): boolean {
+    return this.size === 0
+  }
+
+  get size(): number {
+    return this._size
+  }
+
+  *[Symbol.iterator](): Iterator<T> {
+    const sortArr = sortBy(this.arr, () => Math.random() - 0.5)
+    for (let item of sortArr) {
+      yield item
+    }
+  }
+
+  add(item: T): void {
+    this.arr.push(item)
+    this._size++
+  }
+}
+
+it('1.3.34', () => {
+  const bag = new RandomBag<number>()
+  bag.add(0)
+  bag.add(1)
+  bag.add(2)
+  console.log([...bag])
+})
---
+++ src/util/chapter01/__tests__/1.3.35.test.ts
@@ -0,0 +1,16 @@
+/*
+1.3.35　随机队列。随机队列能够存储一组元素并支持表 1.3.11 中的 API：
+
+　　　表 1.3.11　泛型随机队列的 API
+
+public class RandomQueue<Item>
+             RandomQueue()	创建一条空的随机队列
+   boolean   isEmpty()	队列是否为空
+      void   enqueue(Item item)	添加一个元素
+      Item   dequeue()	删除并随机返回一个元素（取样且不放回）
+      Item   sample()	随机返回一个元素但不删除它（取样且放回）
+　　　编写一个 RandomQueue 类来实现这份 API。提示：使用（能够动态调整大小的）数组表示数据。删除一个元素时，随机交换某个元素（索引在 0 和 N-1 之间）和末位元素（索引为N-1）的位置，然后像 ResizingArrayStack 一样删除并返回末位元素。编写一个用例，使用 RandomQueue<Card> 在桥牌中发牌（每人 13 张）。
+TODO 应该有某种更好的方式
+ */
+
+it('1.3.35', () => {})
