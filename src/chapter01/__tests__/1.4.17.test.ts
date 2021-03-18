e80aabf6d96465ecf958b9bd5c4312903759c623
rxliuli <rxliuli@gmail.com>
2021-03-18 16:55:06 +0800

    feat: 实现 1.4.17 的另一个版本：要求顺序的情况

--- src/chapter01/__tests__/1.4.17.test.ts
+++ src/chapter01/__tests__/1.4.17.test.ts
@@ -2,6 +2,8 @@
 1.4.17　最遥远的一对（一维）。编写一个程序，给定一个含有 N 个 double 值的数组 a[]，在其中找到一对最遥远的值：两者之差（绝对值）最大的两个数。程序在最坏情况下所需的运行时间应该是线性级别的。
  */
 
+import { RandomUtil } from '../RandomUtil'
+
 describe('1.4.17', () => {
   function testF(f: (arr: number[]) => [number, number]) {
     expect(f([2, 1, 3, 5, 3])).toEqual([1, 5])
@@ -50,7 +52,44 @@ describe('1.4.17', () => {
 
     testF(f)
   })
-  it('要求两个数的顺序', () => {
+  describe('要求两个数的顺序', () => {
     // TODO 股票什么时候买入、卖出是最赚钱的
+    function f1(arr: number[]): [number, number] {
+      return arr
+        .flatMap((v, i) => arr.slice(i + 1).map((v2) => [v2 - v, v, v2]))
+        .sort(([d1], [d2]) => d2 - d1)[0]
+        .slice(1) as [number, number]
+    }
+
+    it('暴力方法', () => {
+      expect(f1([10, 1, 8, 5, 7, 9, 5])).toEqual([1, 9])
+    })
+    it('使用遍历记录最大和最小值', async () => {
+      /**
+       * 三个临时变量
+       * res: 上一次的最小值
+       * l: 临时的左边的值，遇到更小的就会更换
+       * 如果判断当前的值 - l 会更大的话就替换掉 res
+       * @param arr
+       */
+      function f(arr: number[]): [number, number] {
+        let [l, r] = arr
+        let temp = l
+        for (let i = 0; i < arr.length; i++) {
+          const v = arr[i]
+          if (v <= temp) {
+            temp = v
+          } else if (v - temp > r - l) {
+            l = temp
+            r = v
+          }
+        }
+        return [l, r]
+      }
+
+      expect(f1([10, 1, 8, 5, 7, 9, 5])).toEqual([1, 9])
+      const arr = RandomUtil.array(100, () => RandomUtil.integer(1000))
+      expect(f(arr)).toEqual(f1(arr))
+    })
   })
 })
