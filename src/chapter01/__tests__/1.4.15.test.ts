39b2b2ed1cdaa8a786008b77bcaf4ab01e73e088
rxliuli <rxliuli@gmail.com>
2021-03-18 11:44:04 +0800

    feat: 实现 1.4.15

--- src/chapter01/__tests__/1.4.15.test.ts
+++ src/chapter01/__tests__/1.4.15.test.ts
@@ -2,7 +2,7 @@
 1.4.15　快速 3-sum。作为热身，使用一个线性级别的算法（而非基于二分查找的线性对数级别的算法）实现 TwoSumFaster 来计算已排序的数组中和为 0 的整数对的数量。用相同的思想为 3-sum 问题给出一个平方级别的算法。
  */
 
-import { sortBy } from '@liuli-util/array'
+import { testTwoSum } from './1.4.2.test'
 
 describe('1.4.15', () => {
   it('实现快速的 twoSumFaster', () => {
@@ -12,8 +12,34 @@ describe('1.4.15', () => {
      * @param sum
      */
     function twoSumFaster(arr: number[], sum: number): [number, number][] {
-      arr = sortBy(arr)
-      return []
+      let l = 0
+      let r = arr.length - 1
+      const res: [number, number][] = []
+      while (l < r) {
+        const v = arr[l] + arr[r]
+        if (v === sum) {
+          res.push([l, r])
+          if (arr[l] === arr[l + 1] && arr[r] === arr[r - 1]) {
+            res.push([l, r - 1])
+            res.push([l + 1, r])
+            l++
+            r--
+          } else if (arr[l] === arr[l + 1]) {
+            l++
+          } else if (arr[r] === arr[r - 1]) {
+            r--
+          } else {
+            r--
+          }
+        } else if (v > sum) {
+          r--
+        } else {
+          l++
+        }
+      }
+      return res
     }
+
+    testTwoSum(twoSumFaster)
   })
 })
