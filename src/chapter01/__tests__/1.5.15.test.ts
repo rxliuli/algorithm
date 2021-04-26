06423f61092377457803f654da54eee867629b5d
rxliuli <rxliuli@gmail.com>
2021-04-26 16:04:54 +0800

    refactor: 优化 1.5.15 的 genOpList，尝试找到找到一个满足每次除都除不尽，直到除到 2 为止的数字

--- src/chapter01/__tests__/1.5.15.test.ts
+++ src/chapter01/__tests__/1.5.15.test.ts
@@ -89,13 +89,121 @@ describe('1.5.15', () => {
     }
 
     it('对比 genNodeCountList 与 computeNodeCountList', () => {
-      const height = RandomUtil.integer(10)
+      const height = RandomUtil.integer(1, 10)
       const tree = genTree(height)
       expect(computeNodeCountList(tree)).toEqual(genNodeCountList(height))
     })
     it('测试 genOpList 出现的堆栈溢出错误', () => {
       //genOpList 不支持比 2^16 更大的数
-      console.log(Math.pow(2, 10))
+      expect(() => genOpList(Math.pow(2, 18))).toThrowError()
+    })
+    it('使用递归的 genOpList', () => {
+      /**
+       * 虽然使用了递归，但由于是二分分割不断缩小，所以实际上栈的层级并不会很大
+       * @param num
+       */
+      function genOpList(num: number) {
+        const res: [number, number][] = []
+
+        function f(arr: number[]) {
+          if (arr.length <= 2) {
+            res.push(arr as [number, number])
+            return
+          }
+
+          const temp: number[] = []
+          for (let i = 0; i < arr.length; i += 2) {
+            const v = arr[i]
+            temp.push(v)
+            const next = arr[i + 1]
+            if (next) {
+              res.push([v, next])
+            }
+          }
+
+          f(temp)
+        }
+
+        f(
+          Array(num)
+            .fill(0)
+            .map((_, i) => i),
+        )
+
+        return res
+      }
+
+      console.log(genOpList(7))
+      const height = RandomUtil.integer(1, 10)
+      const tree = genTree(height)
+      expect(computeNodeCountList(tree)).toEqual(genNodeCountList(height))
+    })
+    /**
+     * 这种数字的有趣之处在于可以让树生成的略微扭曲，最后一个节点会直接挂到根上
+     */
+    it('找到一个满足每次除都除不尽，直到除到 2 为止的数字', () => {
+      /**
+       * 检查一个数字是否满足每次除都除不尽，直到除到 2 为止
+       * @param n
+       */
+      function check(n: number): boolean {
+        function isOdd(n: number): boolean {
+          return n % 2 === 1
+        }
+
+        function f(n: number): boolean {
+          if (n === 2) {
+            return true
+          }
+          if (!isOdd(n)) {
+            return false
+          }
+          return f(Math.ceil(n / 2))
+        }
+
+        if (n <= 2) {
+          return false
+        }
+        return f(n)
+      }
+
+      console.log(check(5))
+      const arr = RandomUtil.array(100, (i) => i + 1).filter(check)
+      console.log(arr.slice(0, 10))
+
+      /**
+       * 生成一个上面的函数需要的数字
+       * @param n
+       */
+      function gen(n: number): number {
+        function f(n: number): number {
+          if (n === 2) {
+            return n
+          }
+          return f(n - 1) * 2 - 1
+        }
+
+        if (n < 0) {
+          throw new Error()
+        }
+        return f(n + 3)
+      }
+
+      /**
+       * 通过数学推导的方式得到了下面的实现，一个是递归 - 1，一个是计算幂值 + 1，但结果相等，挺神奇的
+       * @link https://photos.google.com/photo/AF1QipOSaFYy7eJRmjV1YXMOCsN4g9BRJozogc5_b0md
+       * @param n
+       */
+      function gen2(n: number): number {
+        return Math.pow(2, n + 1) + 1
+      }
+
+      console.log(gen(0))
+      console.log(gen2(0))
+      arr.forEach((v, i) => {
+        expect(gen(i)).toBe(v)
+        expect(gen2(i)).toBe(v)
+      })
     })
   })
 })
