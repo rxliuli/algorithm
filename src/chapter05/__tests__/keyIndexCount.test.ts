1dc9a9cfcb27002fbd92c6f894158fb5524b2499
rxliuli <rxliuli@gmail.com>
2021-06-29 12:17:46 +0800

    feat: 实现键索引排序和低位优先的字符串排序，抽离通用方法 sortByCount

--- src/chapter05/__tests__/keyIndexCount.test.ts
+++ src/chapter05/__tests__/keyIndexCount.test.ts
@@ -1,32 +1,63 @@
 import { sortBy } from '@liuli-util/array'
 
+/**
+ * 键索引排序
+ * @param cards
+ * @param getKey 必须大于等于 1
+ * @param valueCount 索引数组的长度，{@param getKey} 获取的值必须小于等于这个值，理想情况下除了首项应该占满，字符表应该就是在这里使用的
+ */
+export function sortByCount<T>(
+  cards: T[],
+  getKey: (value: T) => number,
+  valueCount: number,
+) {
+  const count = new Array(valueCount + 1).fill(0)
+  const aux = new Array(cards.length)
+  for (let card of cards) {
+    count[getKey(card)]++
+  }
+  for (let i = 1; i < count.length; i++) {
+    count[i] += count[i - 1]
+  }
+  for (let card of cards) {
+    aux[count[getKey(card) - 1]++] = card
+  }
+  return aux
+}
+
 it('键索引计数法', () => {
   interface User {
     name: string
     group: number
   }
 
+  /**
+   * @link https://photos.google.com/photo/AF1QipOK7U5e6DVFlu9KpYfvv70dtwkzyZBg-GjZlofH
+   * @param users
+   * @param R
+   */
   function sortUser(users: User[], R: number) {
-    const count = new Array(R + 1).fill(0)
-    //统计频率
-    for (let user of users) {
-      count[user.group]++
-    }
-    // console.log(count)
-    //将频率转换为下标数组
-    for (let i = 1; i < count.length; i++) {
-      count[i] += count[i - 1]
-    }
-    // console.log(count)
-    //按照下标将数组分组
-    const res = new Array(users.length)
-    for (let user of users) {
-      //这步其实做了很多操作
-      //获取当前分组的下标，然后写入，最后将分组的下标+1
-      res[count[user.group - 1]++] = user
-    }
-    // console.log(res)
-    return res
+    return sortByCount(users, (value) => value.group, R)
+    // const count = new Array(R + 1).fill(0)
+    // //统计频率
+    // for (let user of users) {
+    //   count[user.group]++
+    // }
+    // // console.log(count)
+    // //将频率转换为下标数组
+    // for (let i = 1; i < count.length; i++) {
+    //   count[i] += count[i - 1]
+    // }
+    // // console.log(count)
+    // //按照下标将数组分组
+    // const res = new Array(users.length)
+    // for (let user of users) {
+    //   //这步其实做了很多操作
+    //   //获取当前分组的下标，然后写入，最后将分组的下标+1
+    //   res[count[user.group - 1]++] = user
+    // }
+    // // console.log(res)
+    // return res
   }
 
   const R = 4
---
+++ src/chapter05/__tests__/lowOrderFirst.test.ts
@@ -0,0 +1,148 @@
+import { sortBy } from '@liuli-util/array'
+import { RandomUtil } from '../../chapter01/RandomUtil'
+import { Random } from 'mockjs'
+import { ArrayUtil } from '../../chapter01/ArrayUtil'
+import { sortByCount } from './keyIndexCount.test'
+
+describe('低位优先', () => {
+  // function sortByW(arr: string[], w: number) {
+  //   const len = arr.length
+  //   const aux = new Array(len)
+  //   //这里其实就是默认已知可能出现的字符都在 0-256 之间了
+  //   const R = 256
+  //   const count = new Array(R).fill(0)
+  //   //统计频率
+  //   for (let s of arr) {
+  //     count[s.charCodeAt(w)]++
+  //   }
+  //   // console.log(count['5'.charCodeAt(0)])
+  //   //将频率转换为下标
+  //   for (let i = 1; i < count.length; i++) {
+  //     count[i] += count[i - 1]
+  //   }
+  //   //复制到 aux
+  //   for (let s of arr) {
+  //     aux[count[s.charCodeAt(w) - 1]++] = s
+  //   }
+  //   // console.log(aux)
+  //   //回写到 arr
+  //   for (let i = 0; i < len; i++) {
+  //     arr[i] = aux[i]
+  //   }
+  //   return arr
+  // }
+
+  /**
+   * 低位优先的字符串排序
+   * 其实想法是将低位排序之后，再排序高位的，那么高位相同的字符串低位仍然是有序的，只不过与高位排序不同之处在于排序的顺序不同，优先保证同一高位的低位有序罢了
+   * @param arr
+   * @param W
+   */
+  function sortByLowOrderFirst(arr: string[], W: number) {
+    let res = arr
+    for (let w = W - 1; w >= 0; w--) {
+      res = sortByCount(res, (s) => s.charCodeAt(w), 256)
+    }
+    return res
+  }
+
+  it('基本示例', () => {
+    const arr = ['5413', '4628', '1907', '3825']
+    const res = sortByLowOrderFirst([...arr], 4)
+    console.log(res)
+    expect(res).toEqual(sortBy(arr))
+  })
+  it('随机数据测试', () => {
+    const w = 10
+    const arr = RandomUtil.array(10, () =>
+      RandomUtil.array(w, () =>
+        String.fromCharCode(Random.integer(46, 256)),
+      ).join(''),
+    )
+    expect(sortByLowOrderFirst([...arr], w)).toEqual(sortBy(arr))
+  })
+})
+
+describe('扑克牌排序', () => {
+  enum CardColorEnum {
+    /**
+     * 红桃
+     */
+    Hearts = 1,
+    /**
+     * 方块
+     */
+    Cube,
+    /**
+     * 梅花
+     */
+    PlumFlower,
+    /**
+     * 黑桃
+     */
+    Spades,
+  }
+
+  enum CardValueEnum {
+    $A = 1,
+    $2,
+    $3,
+    $4,
+    $5,
+    $6,
+    $7,
+    $8,
+    $9,
+    $10,
+    $J,
+    $Q,
+    $K,
+  }
+
+  interface Card {
+    color: CardColorEnum
+    value: CardValueEnum
+  }
+
+  /**
+   * 排序扑克牌
+   * 基本思路
+   * 将之按照值分成 13 堆，再根据花色分成四堆
+   * @param cards
+   */
+  function sortCard(cards: Card[]) {
+    let res = sortByCount(cards, (card) => card.value, 13)
+    // console.log('value: ', res)
+    res = sortByCount(res, (card) => card.color, 4)
+    // console.log('color: ', res)
+    return res
+  }
+
+  it('基本示例', () => {
+    const cards: Card[] = [
+      { color: CardColorEnum.Cube, value: CardValueEnum.$3 },
+      { color: CardColorEnum.Cube, value: CardValueEnum.$4 },
+      { color: CardColorEnum.Cube, value: CardValueEnum.$A },
+      { color: CardColorEnum.Cube, value: CardValueEnum.$2 },
+    ]
+
+    expect(sortCard(cards)).toEqual(
+      sortBy(cards, (item) => item.color * 100 + item.value),
+    )
+  })
+  it('生成一副完整的扑克牌测试', () => {
+    const cards = RandomUtil.array(4, (i) =>
+      RandomUtil.array(
+        13,
+        (k) =>
+          ({
+            color: ((i + 1) as unknown) as CardColorEnum,
+            value: ((k + 1) as unknown) as CardValueEnum,
+          } as Card),
+      ),
+    ).flat()
+    const shuffle = [...ArrayUtil.shuffle(cards)]
+    expect(shuffle).not.toEqual(cards)
+    expect(sortCard(shuffle)).toEqual(cards)
+  })
+})
